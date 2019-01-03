80cc9d736b27522800f332ee6145ce4aeb23c614
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2019-01-03 20:55:01 +0000

    Added put route

--- routes/apis.js
+++ routes/apis.js
@@ -4,26 +4,32 @@ const User = require("../models/user");
 
 
 // get a list of users from the db
-router.get("/users", function(req, res) {
+router.get("/users", function(req, res, next) {
     res.send({ type: "GET" });
 });
 
 // add a new user to the db
-router.post("/users", function(req, res) {
+router.post("/users", function(req, res, next) {
     User.create(req.body).then(function(user) {
             console.log(user);
-            res.send({user});
-    });
+            res.send(user);
+    }).catch(next);
 });
 
 // update a user in the db
-router.put("/users/:id", function(req, res) {
-    res.send({ type: "PUT" });
+router.put("/users/:id", function(req, res, next) {
+    User.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+        User.findOne({_id:req.params.id}).then(function(user){
+            res.send(user);
+        });
+    });
 });
 
 // delete a user in the db
-router.delete("/users/:id", function(req, res) {
-    res.send({ type: "DELETE" });
+router.delete("/users/:id", function(req, res, next) {
+    User.findByIdAndRemove({_id: req.params.id}).then(function(user){
+       res.send(user); 
+    });
 });
 
 
