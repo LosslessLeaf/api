c6e43d92e1fb7f2ae5317d1c625ab9e39542d83f
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2019-01-03 18:00:30 +0000

    Added post router

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 // set up express app
 const app = express();
 
+
+app.use(bodyParser.json());
 app.use('/api', require("./routes/apis"));
 
 // listen for requests
